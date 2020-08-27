061621e6acf374b69238934c4fd9cd3d0ea6bb1d
James Kinyua <jameskinyua590@gmail.com>
2020-08-27 18:04:55 +0300

    wire up cart to dump data to a json file

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -40,7 +41,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const { productId: prodId } = req.body;
-  console.log(prodId);
+  Product.findById(prodId.trim(), (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect("/cart");
 };
 
