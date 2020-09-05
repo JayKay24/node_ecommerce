274a78e252e770bebed54418c240c3a5b4c2440d
James Kinyua <jameskinyua590@gmail.com>
2020-09-05 19:51:52 +0300

    leverage sequelize to create a new product

--- models/product.js
+++ models/product.js
@@ -1,60 +1,18 @@
-// const fs = require("fs");
-// const path = require("path");
-const db = require("../util/database");
-const Cart = require("./cart");
+const Sequelize = require("sequelize");
 
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   "data",
-//   "products.json"
-// );
+const sequelize = require("../util/database");
 
-// const getProductsFromFile = (cb) => {
-//   fs.readFile(p, (err, fileContent) => {
-//     if (err) {
-//       cb([]);
-//     } else {
-//       cb(JSON.parse(fileContent));
-//     }
-//   });
-// };
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: { type: Sequelize.DOUBLE, allowNull: false },
+  imageUrl: { type: Sequelize.STRING, allowNull: false },
+  description: { type: Sequelize.STRING, allowNull: false },
+});
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    id && (this.id = id.trim());
-    this.title = title.trim();
-    this.imageUrl = imageUrl.trim();
-    this.description = description.trim();
-    this.price = +price;
-  }
-
-  save() {
-    return db.execute(
-      `INSERT INTO products (title, price , imageUrl, description) values (?, ?, ?, ?)`,
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  // static fetchAll(cb) {
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  // static findById(id, cb) {
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-
-  static deleteById(id) {
-    //   id = id.trim();
-    //   getProductsFromFile((products) => {
-    //     const product = products.find((p) => p.id === id);
-    //     const updatedProducts = products.filter((p) => p.id !== id);
-    //     fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-    //       if (!err) {
-    //         Cart.deleteProduct(id, product.price);
-    //       }
-    //     });
-    //   });
-  }
-};
+module.exports = Product;
