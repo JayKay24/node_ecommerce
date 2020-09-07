3eb352f5ce4fde688d8b2a047e8c16b66e48f15b
James Kinyua <jameskinyua590@gmail.com>
2020-09-07 18:29:26 +0300

    modify getEditProduct & getProduct to leverage sequelize magic methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -29,15 +29,17 @@ exports.getEditProduct = (req, res, next) => {
 
   const { productId: prodId } = req.params;
 
-  Product.findByPk(prodId)
-    .then((product) =>
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0];
       res.render("admin/edit-product", {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
         product,
-      })
-    )
+      });
+    })
     .catch((err) => console.log(err));
 };
 
@@ -67,7 +69,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) =>
       res.render("admin/products", {
         prods: products,
