d23d64875ef0369ea3269cf2911339dfe179bcd5
James Kinyua <jameskinyua590@gmail.com>
2020-09-14 10:29:41 +0300

    leverage sequelize to create an order from the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -107,6 +106,26 @@ exports.getOrders = (req, res, next) => {
     path: "/orders",
     pageTitle: "Your Orders",
   });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user.createOrder().then((order) => {
+        return order.addProducts(
+          products.map((product) => {
+            product.orderItem = { quantity: product.cartItem.quantity };
+            return product;
+          })
+        );
+      });
+    })
+    .then(() => res.redirect("/orders"))
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
