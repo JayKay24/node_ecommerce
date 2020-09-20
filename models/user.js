6493d240b251d0b9d828f82cb72c47758ea2c027
James Kinyua <jameskinyua590@gmail.com>
2020-09-20 20:35:06 +0300

    leverage mongodb to display cart items

--- models/user.js
+++ models/user.js
@@ -54,6 +54,26 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((item) => item.productId);
+
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((product) => {
+          return {
+            ...product,
+            quantity: this.cart.items.find(
+              (i) => i.productId.toString() === product._id.toString()
+            ).quantity,
+          };
+        });
+      });
+  }
+
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
