3758cc53496ef574fba3cd1b7590e6348628d6ea
James Kinyua <jameskinyua590@gmail.com>
2020-09-21 18:21:10 +0300

    get all orders from mongodb

--- models/user.js
+++ models/user.js
@@ -89,9 +89,14 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("order")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: { _id: new ObjectId(this._id), name: this.name },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
@@ -100,7 +105,16 @@ class User {
             { _id: new ObjectId(this._id) },
             { $set: { cart: { items: [] } } }
           );
-      });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
   }
 
   save() {
