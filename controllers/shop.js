85d107d6100de01cad2fff72a8199b147124b5c4
James Kinyua <jameskinyua590@gmail.com>
2020-09-25 18:19:05 +0300

    leverage mongoose to fetch all orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,8 +76,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       console.log(orders);
       res.render("shop/orders", {
