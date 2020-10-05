8d8a34e0623b8cd49b0a8b54bd8c215c427d3fbb
James Njuguna <jamesnjuguna@Jamess-MacBook-Pro.local>
2020-10-05 18:35:42 +0300

    add initial signup implementation

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
@@ -19,6 +27,19 @@ exports.postLogin = (req, res, next) => {
       });
     })
     .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const { email, password, confirmPassword } = req.body;
+
+  User.findOne({ email }).then((userDoc) => {
+    if (userDoc) {
+      return res.redirect("/signup");
+    }
+
+    const user = new User({ name: 'James', email, password, cart: { items: [] } });
+    return user.save();
+  }).then(() => res.redirect("/login")).catch(err => console.log(err))
 };
 
 exports.postLogout = (req, res, next) => {
