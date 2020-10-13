2b92bed1f99a285f61e5d16aab13d89177a1f4b9
James Njuguna <jamesnjuguna@Jamess-MacBook-Pro.local>
2020-10-13 17:48:03 +0300

    create a form to submit email for password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -100,8 +100,7 @@ exports.postSignup = (req, res, next) => {
               to: email,
               from: "jameskinyua590test@gmail.com",
               subject: "Signup succeeded!",
-              html:
-                "<h1>I've sent this from my node.js app!</h1>",
+              html: "<h1>I've sent this from my node.js app!</h1>",
             });
           })
           .catch((err) => console.log(err));
@@ -114,5 +113,20 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
   });
 };
