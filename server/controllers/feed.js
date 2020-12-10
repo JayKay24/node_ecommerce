292a7588ea65ded9a366883df64ced4963b0dbfc
James Njuguna <jameskinyua590@gmail.com>
2020-12-10 21:43:49 +0300

    implement pagination logic on client and server

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -4,13 +4,24 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  const { page: currentPage } = req.query || 1;
+  const perPage = 2;
+  let totalItems;
+
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find().skip((currentPage - 1) * perPage);
+    })
     .then((posts) => {
-      res.status(200).json({ posts, message: "Fetched posts successfully" });
+      res
+        .status(200)
+        .json({ posts, message: "Fetched posts successfully", totalItems });
     })
     .catch((err) => {
       if (!err.statusCode) {
-        err.statusCode = 500;
+        err.statusCode = 200;
       }
       next(err);
     });
