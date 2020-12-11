014971b4dcbc813b048411b7186740cceed22daa
James Njuguna <jameskinyua590@gmail.com>
2020-12-11 17:59:57 +0300

    clear post-user relations when deleting a post

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const User = require("../models/user");
+const user = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const { page: currentPage } = req.query || 1;
@@ -174,7 +175,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((result) => {
       res.status(200).json({ message: "Deleted Post" });
     })
     .catch((err) => {
