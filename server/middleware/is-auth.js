88fb24b2f93d46071d0670491c75f45a725102c7
James Njuguna <jameskinyua590@gmail.com>
2020-12-13 15:20:30 +0300

    emit and consume events using socket.io

--- server/middleware/is-auth.js
+++ server/middleware/is-auth.js
@@ -1,29 +1,25 @@
-const jwt = require("jsonwebtoken");
-
-const notAuthenticated = (msg) => {
-  const error = new Error(msg);
-  error.statusCode = 401;
-  throw error;
-};
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const authHeader = req.get("Authorization");
+  const authHeader = req.get('Authorization');
   if (!authHeader) {
-    notAuthenticated("Not Authenticated");
-  }
-  const [, token] = req.get("Authorization").split(" ");
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error) {
-    error.statusCode = 500;
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
     throw error;
   }
-
-  if (!decodedToken) {
-    notAuthenticated("Not authenticated");
+  const token = authHeader.split(' ')[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, 'somesupersecretsecret');
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
   }
-
+  if (!decodedToken) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
   req.userId = decodedToken.userId;
   next();
 };
